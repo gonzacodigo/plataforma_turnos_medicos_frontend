322b4738144c93a0a4bef3a4a10058570aedee7b
Daniel Fabián <103130176+Dofdanicho@users.noreply.github.com>
2025-01-30 23:49:41 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import '../public/css/estilos.css'
+import '../public/css/bootstrap.min.css'
+import '../public/js/bootstrap.bundle.min.js'
+import '../public/js/bootstrap.min.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
